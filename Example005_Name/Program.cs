f42cdfd876b292055baa3ce7f134c1127547bceb
Katya <fk.1900@mail.ru>
2023-12-03 23:50:07 +0300

    Example005_Name

---
+++ Example005_Name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("User Name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Yea, it's Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
