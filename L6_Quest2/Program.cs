3d4ad80bb9c6014302d6852f9448fe7324a1dd79
Katya <fk.1900@mail.ru>
2023-12-25 01:14:52 +0300

    заглавные в строчные

---
+++ L6_Quest2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "vdFEnfWE1!-";
+
+        string result = input.ToLower();
+
+        Console.WriteLine(result);
+    }
+}
