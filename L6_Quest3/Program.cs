061333ff051ad4df92e4cadefdc0ec81f0d2d1c4
Katya <fk.1900@mail.ru>
2023-12-25 01:28:04 +0300

    палиндром

---
+++ L6_Quest3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "домик";
+
+        bool isPalindrome = IsPalindrome(input);
+
+        Console.WriteLine(isPalindrome ? "Yes" : "No");
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        string normalized = new
+        string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+
+        return normalized.SequenceEqual(normalized.Reverse());
+    }
+}
