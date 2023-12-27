45ae65f2daecdf53d174b3bef01b3b55525dc6a6
Katya <fk.1900@mail.ru>
2023-12-27 23:28:55 +0300

    3 задача итоговая

---
+++ EndProg3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+static int[] v = {6, 3, 93, 1, 36};
+        static int g = v.Length - 1;
+        static void Main(string[] args) 
+        {
+            recursion(g);
+            Console.ReadKey();
+        }
+        static void recursion(int x) 
+        {
+            int n = 0;
+            int[] shiftx = new int[5];
+            if (x >= 0) 
+            {
+                shiftx[n] = v[x];
+                Console.Write(shiftx[n]);
+                n++;
+                x--;
+            } else 
+            {
+                return;
+            }
+            recursion(x);
+        }
+}
